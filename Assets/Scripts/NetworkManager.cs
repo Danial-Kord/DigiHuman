01dd72c11f49966f53c8148a19f2bb31e5531304
Danial <danial.kordmodanlou@gmail.com>
2022-03-12 20:38:46 +0330

    [feat]: uploading file to server test successful

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -1,14 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class NetworkManager : MonoBehaviour
 {
+
+    [Header("Server")]
+    [SerializeField] private string serverURL; //server URL
     
+    //for testing in engine only
+#if UNITY_EDITOR
+    [Header("Debug")] 
+    [SerializeField] private bool enableDebug; //if this is true we are in debug mode!
+    [SerializeField] private string filePath; //for testing system
+#endif
+
+    private void Start()
+    {
+        //for testing in engine only
+#if UNITY_EDITOR
+        if (enableDebug)
+        {
+            UploadFile(filePath,serverURL);
+        } 
+#endif
+    }
+
+    //starting coroutine for sending ASync to server
     public void UploadFile(string localFileName, string uploadURL)
     {
+        
         StartCoroutine(UploadFileCo(localFileName, uploadURL));
     }
+    
+    //Async file uploader
     IEnumerator UploadFileCo(string localFileName, string uploadURL)
     {
         WWW localFile = new WWW("file:///" + localFileName);
@@ -24,7 +51,7 @@ public class NetworkManager : MonoBehaviour
         // version 1
         //postForm.AddBinaryData("theFile",localFile.bytes);
         // version 2
-        postForm.AddBinaryData("theFile",localFile.bytes,localFileName,"text/plain");
+        postForm.AddBinaryData("file",localFile.bytes,localFileName,"text/plain");
         WWW upload = new WWW(uploadURL,postForm);        
         yield return upload;
         if (upload.error == null)
