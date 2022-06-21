0432dcf31043a869525aabc39ecaf5693fb096a3
Danial <danial.kordmodanlou@gmail.com>
2022-06-21 14:08:28 +0430

    [feat]: Animation save system added

--- Assets/Scripts/BackgroundImageManager.cs
+++ Assets/Scripts/BackgroundImageManager.cs
@@ -43,7 +43,7 @@ public class BackgroundImageManager : MonoBehaviour
     private Sprite LoadNewSprite(string FilePath, float PixelsPerUnit = 100.0f) {
    
         // Load a PNG or JPG image from disk to a Texture2D, assign this texture to a new sprite and return its reference
-        Texture2D SpriteTexture = LoadTexture(FilePath);
+        Texture2D SpriteTexture = FileManager.LoadTexture(FilePath);
         Sprite NewSprite = Sprite.Create(SpriteTexture, new Rect(0, 0, SpriteTexture.width, SpriteTexture.height),new Vector2(0,0), PixelsPerUnit);
  
         return NewSprite;
@@ -76,22 +76,7 @@ public class BackgroundImageManager : MonoBehaviour
     {
         resultImageCanvas.SetActive(true);
     }
-    private Texture2D LoadTexture(string FilePath) {
- 
-        // Load a PNG or JPG file from disk to a Texture2D
-        // Returns null if load fails
- 
-        Texture2D Tex2D;
-        byte[] FileData;
- 
-        if (File.Exists(FilePath)){
-            FileData = File.ReadAllBytes(FilePath);
-            Tex2D = new Texture2D(2, 2);           // Create new "empty" texture
-            if (Tex2D.LoadImage(FileData))           // Load the imagedata into the texture (size is set automatically)
-                return Tex2D;                 // If data = readable -> return texture
-        }  
-        return null;                     // Return null if load failed
-    }
+
 
 
     
