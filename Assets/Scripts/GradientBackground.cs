7a86411846579861064fc52e1aa64e553850ed7d
Danial <danial.kordmodanlou@gmail.com>
2022-06-23 18:03:12 +0430

    [feat]: Background effect added

--- Assets/Scripts/GradientBackground.cs
+++ Assets/Scripts/GradientBackground.cs
@@ -19,12 +19,118 @@ public class GradientBackground : MonoBehaviour
 
     private IEnumerator PlayAnimation()
     {
+        float R = minRGBValue;
+        float G = minRGBValue;
+        float B = maxRGBValue;
+        bool RChanging = true;
+        bool GChanging = false;
+        bool BChanging = false;
+        int direction = -1;
+        Color color = new Color(R, G, B);
         
         while (true)
         {
             if (!this.gameObject.activeSelf)
                 yield return null;
-            
+            if (RChanging)
+            {
+                if (direction == -1 && R < minRGBValue)
+                {
+                    R = minRGBValue;
+                    RChanging = false;
+                    direction = +1;
+                    if (G == minRGBValue)
+                    {
+                        GChanging = true;
+                    }
+                    else
+                        BChanging = true;
+                }
+                else if (direction == +1 && R > maxRGBValue)
+                {
+                    R = maxRGBValue;
+                    RChanging = false;
+                    direction = -1;
+                    if (G == maxRGBValue)
+                    {
+                        GChanging = true;
+                    }
+                    else
+                        BChanging = true;
+                }
+                else
+                {
+                    R += Time.deltaTime * speed * direction;
+                }
+            }
+            else if (GChanging)
+            {
+                if (direction == -1 && G < minRGBValue)
+                {
+                    G = minRGBValue;
+                    GChanging = false;
+                    direction = +1;
+                    if (R == minRGBValue)
+                    {
+                        RChanging = true;
+                    }
+                    else
+                        BChanging = true;
+                }
+                else if (direction == +1 && G > maxRGBValue)
+                {
+                    G = maxRGBValue;
+                    GChanging = false;
+                    direction = -1;
+                    if (R == maxRGBValue)
+                    {
+                        RChanging = true;
+                    }
+                    else
+                        BChanging = true;
+                }
+                else
+                {
+                    G += Time.deltaTime * speed * direction;
+                }
+            }
+            else
+            {
+                if (direction == -1 && B < minRGBValue)
+                {
+                    B = minRGBValue;
+                    BChanging = false;
+                    direction = +1;
+                    if (R == minRGBValue)
+                    {
+                        RChanging = true;
+                    }
+                    else
+                        GChanging = true;
+                }
+                else if (direction == +1 && B > maxRGBValue)
+                {
+                    B = maxRGBValue;
+                    BChanging = false;
+                    direction = -1;
+                    if (R == maxRGBValue)
+                    {
+                        RChanging = true;
+                    }
+                    else
+                        GChanging = true;
+                }
+                else
+                {
+                    B += Time.deltaTime * speed * direction;
+                }
+            }
+
+            color.r = R / 255.0f;
+            color.g = G / 255.0f;
+            color.b = B / 255.0f;
+            Camera.main.backgroundColor = color;
+            yield return new WaitForEndOfFrame();
         }
     }
 }
